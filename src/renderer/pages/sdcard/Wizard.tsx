45fc379efe30a058eaa288e9577d286d36dfd0bf
Oliver Bell <freshollie@gmail.com>
2021-12-02 00:15:03 +0000

    continue moving sd card logic to backend

--- src/renderer/pages/sdcard/Wizard.tsx
+++ src/renderer/pages/sdcard/Wizard.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
@@ -11,65 +11,62 @@ import React, { useLayoutEffect, useRef, useState } from "react";
 import { isNotNullOrUndefined } from "type-guards";
 import { unzipRaw } from "unzipit";
 
-const radios = [
-  { name: "FlySky Nirvana", target: "nv14.zip" },
-  { name: "Jumper T16", target: "horus.zip" },
-  { name: "Jumper T18", target: "horus.zip" },
-  { name: "Jumper T-Lite", target: "taranis-x7.zip" },
-  { name: "Jumper T12", target: "taranis-x7.zip" },
-  { name: "Jumper T8", target: "taranis-x7.zip" },
-  { name: "FrSky Horus X10", target: "horus.zip" },
-  { name: "FrSky Horus X12s", target: "horus.zip" },
-  { name: "FrSky QX7", target: "taranis-x7.zip" },
-  { name: "FrSky X9D", target: "taranis-x9.zip" },
-  { name: "FrSky X9D Plus", target: "taranis-x9.zip" },
-  { name: "FrSky X9D Plus 2019", target: "taranis-x9.zip" },
-  { name: "FrSky X-Lite", target: "taranis-x7.zip" },
-  { name: "FrSky X9 Lite", target: "taranis-x7.zip" },
-  { name: "Radiomaster TX12", target: "taranis-x7.zip" },
-  { name: "Radiomaster TX16s", target: "horus.zip" },
-];
-
 const SdcardWizard: React.FC = () => {
   const [logs, setLogs] = useState<string[]>([]);
-  const [selectedRadio, setSelectedRadio] = useState<string>();
+  const [selectedTarget, setSelectedTarget] = useState<string>();
   const logBoxRef = useRef<HTMLElement | null>(null);
   const log = (line: string) =>
     setLogs((existingLogs) => existingLogs.concat([line]));
 
-  const { data, error, loading } = useQuery(
+  const sdcardTargetsQuery = useQuery(
     gql(/* GraphQL */ `
-      query SDCardReleaseAssets {
-        repository(owner: "EdgeTX", name: "edgetx-sdcard") {
+      query SdcardTargets {
+        sdcardTargets {
           id
-          release(tagName: "latest") {
-            id
-            releaseAssets(first: 100) {
-              nodes {
-                id
-                name
-                url
-              }
-            }
-          }
+          name
+          tag
         }
       }
-    `),
+    `)
   );
 
-  const sdCardAssets =
-    data?.repository?.release?.releaseAssets.nodes?.filter(
-      isNotNullOrUndefined
-    ) ?? [];
-
-  const radiosWithTargetUrls = sdCardAssets.flatMap((asset) =>
-    radios
-      .filter((radio) => radio.target === asset.name)
-      .map((radio) => ({ ...radio, url: asset.url }))
+  const [pickFolder, pickFolderMutation] = useMutation(
+    gql(/* GraphQL */ `
+      mutation PickSdcardFolder {
+        pickSdcardFolder {
+          id
+          name
+        }
+      }
+    `)
   );
-  const selectedTargetUrl = radiosWithTargetUrls.find(
-    (radio) => radio.name === selectedRadio
-  )?.url;
+
+  const [createWriteJob] = useMutation(
+    gql(/* GraphQL */ `
+      mutation CreateSdcardWriteJob(
+        $folderId: ID!
+        $target: ID!
+        $sounds: String!
+        $clean: Boolean
+      ) {
+        createSdcardWriteJob(
+          target: $target
+          sounds: $sounds
+          clean: $clean
+          folderId: $folderId
+        ) {
+          id
+        }
+      }
+    `)
+  );
+
+  const targets = sdcardTargetsQuery.data?.sdcardTargets;
+  const selectedFolder = pickFolderMutation.data?.pickSdcardFolder?.id;
+
+  const selectedTargetExists = !!targets?.find(
+    (target) => target.id === selectedTarget
+  );
 
   useLayoutEffect(() => {
     if (logBoxRef?.current) {
@@ -83,94 +80,29 @@ const SdcardWizard: React.FC = () => {
       <FormControl
         fullWidth
         sx={{ m: 1 }}
-        error={!!error}
-        disabled={loading || !!error}
+        error={!!sdcardTargetsQuery.error}
+        disabled={sdcardTargetsQuery.loading || !!sdcardTargetsQuery.error}
       >
         <InputLabel id="select-radio-label">Select radio</InputLabel>
         <Select
           labelId="select-radio-label"
           id="select-radio"
           label="Select radio"
-          value={selectedRadio}
-          onChange={(e) => setSelectedRadio(e.target.value as string)}
+          value={selectedTarget}
+          onChange={(e) => setSelectedTarget(e.target.value as string)}
         >
-          {radiosWithTargetUrls?.map((radio) => (
+          {targets?.map((radio) => (
             <MenuItem key={radio.name} value={radio.name}>
               {radio.name}
             </MenuItem>
           ))}
         </Select>
-        {error && <FormHelperText>Could not load targets</FormHelperText>}
+        {sdcardTargetsQuery.error && (
+          <FormHelperText>Could not load targets</FormHelperText>
+        )}
       </FormControl>
-      <Button
-        disabled={!selectedTargetUrl}
-        onClick={async () => {
-          if (!selectedTargetUrl) {
-            return;
-          }
-          setLogs([]);
-          log("Requesting directory");
-          // TODO with node we will use native-file-system-adapter and pass the node adapter
-          // specifying the directory which was picked using electron dialog
-          const rootDirectory = await window.showDirectoryPicker({
-            id: "edgetx-sdcard",
-          });
-          log("Downloading");
-          const contents = await (
-            await fetch(`http://localhost:8080/${selectedTargetUrl}`)
-          ).blob();
-          log("Reading file bundle");
-          const { entries: zipEntries } = await unzipRaw(contents);
-
-          log("Writing");
-          const total = zipEntries.reduce(
-            (acc, entity) => acc + entity.size,
-            0
-          );
-          let progress = 0;
-          await Promise.all(
-            zipEntries.map(async (file) => {
-              const path = file.name.split("/");
-              const fileName = path[path.length - 1];
-              // ensure the path exists
-              // and recursively head down the tree
-              // to get to the folder where this file needs
-              // to go
-              const fileDirectory = await path
-                .slice(0, path.length - 1)
-                .filter(Boolean)
-                .reduce(
-                  async (prev, directory) =>
-                    prev.then(async (parentDirectory) =>
-                      parentDirectory.getDirectoryHandle(directory, {
-                        create: true,
-                      })
-                    ),
-                  Promise.resolve(rootDirectory)
-                );
-
-              // Only need to save a file if there was something after the folder
-              if (fileName.length > 0) {
-                const fileHandle = await fileDirectory.getFileHandle(fileName, {
-                  create: true,
-                });
-                await (await file.blob())
-                  .stream()
-                  .pipeTo(await fileHandle.createWritable());
-
-                progress += file.size;
-              }
-
-              log(
-                `[WRITING]: ${Math.round(
-                  (progress / total) * 100
-                )}% - ${path.join("/")}`
-              );
-            })
-          );
-          log("Completed");
-        }}
-      >
+      <Button onClick={() => pickFolder()}>Select directory</Button>
+      <Button disabled={!selectedTargetExists} onClick={async () => {}}>
         Make SD
       </Button>
       <Box
--- src/shared/schema/graph/sdcard/index.ts
+++ src/shared/schema/graph/sdcard/index.ts
@@ -1,9 +1,15 @@
 import gql from "graphql-tag";
-import { Resolvers } from "../__generated__";
+import {
+  Resolvers,
+  SdcardWriteFileStatus,
+  SdcardWriteJob,
+  SdcardWriteJobStages,
+} from "../__generated__";
 import * as uuid from "uuid";
 import { GraphQLError } from "graphql";
-import ky from "ky";
-import { unzipRaw } from "unzipit";
+import ky, { DownloadProgress } from "ky";
+import { unzipRaw, ZipEntry } from "unzipit";
+import { PubSub } from "graphql-subscriptions";
 
 const targetsToAssets = [
   { name: "FlySky Nirvana", asset: "nv14.zip" },
@@ -30,13 +36,20 @@ const nameToId = (name: string): string =>
 const typeDefs = gql`
   type Query {
     sdcardTargets: [SdcardTarget!]!
+    sdcardSounds: [String!]!
     folderInfo(id: ID!): FileSystemFolder
-    sdcardWriteJobStatus(jobId: ID!): SdcardWriteJob!
+    sdcardWriteJobStatus(jobId: ID!): SdcardWriteJob
   }
 
   type Mutation {
-    pickFolder: FileSystemFolder
-    createSdcardWriteJob(folderId: ID!, target: ID!, clean: Boolean): Boolean
+    pickSdcardFolder: FileSystemFolder
+    createSdcardWriteJob(
+      folderId: ID!
+      target: ID!
+      sounds: String!
+      clean: Boolean
+    ): SdcardWriteJob!
+    cancelSdcardWriteJob(jobId: ID!): Boolean
   }
 
   type Subscription {
@@ -46,9 +59,28 @@ const typeDefs = gql`
   type SdcardWriteJob {
     id: ID!
     cancelled: Boolean!
-    erased: Boolean
-    downloadProgress: Float!
-    writes: SdcardWriteFileStatus[];
+    stages: SdcardWriteJobStages!
+  }
+
+  type SdcardWriteJobStages {
+    erase: SdcardWriteJobStage
+    download: SdcardWriteJobStage
+    write: SdcardWriteJobWriteStage!
+  }
+
+  type SdcardWriteJobStage {
+    progress: Float!
+    started: Boolean!
+    completed: Boolean!
+    error: String
+  }
+
+  type SdcardWriteJobWriteStage {
+    progress: Float!
+    writes: [SdcardWriteFileStatus!]!
+    started: Boolean!
+    completed: Boolean!
+    error: String
   }
 
   type SdcardWriteFileStatus {
@@ -65,21 +97,47 @@ const typeDefs = gql`
   type SdcardTarget {
     id: ID!
     name: String!
+    tag: String!
   }
 `;
 
 const resolvers: Resolvers = {
   Query: {
     sdcardTargets: async (_, __, { github }) => {
-      const sdcardAssets = (
-        await github("GET /repos/{owner}/{repo}/releases/latest")
-      ).data.assets;
+      const release = (
+        await github("GET /repos/{owner}/{repo}/releases/latest", {
+          owner: "EdgeTX",
+          repo: "edgetx-sdcard",
+        })
+      ).data;
 
+      const sdcardAssets = release.assets;
       return sdcardAssets.flatMap((asset) =>
         targetsToAssets
           .filter((radio) => radio.asset === asset.name)
-          .map((radio) => ({ ...radio, id: nameToId(radio.name) }))
+          .map((radio) => ({
+            ...radio,
+            id: nameToId(radio.name),
+            tag: release.tag_name,
+          }))
       );
+    },
+    sdcardSounds: async (_, __, { github }) => {
+      const assets = (
+        await github("GET /repos/{owner}/{repo}/releases/latest", {
+          owner: "EdgeTX",
+          repo: "edgetx-sdcard-sounds",
+        })
+      ).data.assets;
+
+      return assets
+        .filter((asset) => asset.name.includes("edgetx-sdcard-sounds-"))
+        .map((asset) =>
+          asset.name
+            .split("edgetx-sdcard-sounds-")[1]
+            .split("-")[0]
+            .toUpperCase()
+        );
     },
     folderInfo: (_, { id }) => {
       const handle = directories.find(
@@ -88,10 +146,33 @@ const resolvers: Resolvers = {
 
       return handle ? { id, name: handle.name } : null;
     },
+    sdcardWriteJobStatus: (_, { jobId }) => getSdcardJob(jobId) ?? null,
   },
-
+  Subscription: {
+    sdcardWriteJobUpdates: {
+      subscribe: (_, { jobId }) => ({
+        [Symbol.asyncIterator]() {
+          return jobUpdates.asyncIterator<SdcardWriteJob>(jobId);
+        },
+      }),
+      resolve: (value: SdcardWriteJob) => value,
+    },
+  },
   Mutation: {
-    pickFolder: async () => {
+    cancelSdcardWriteJob: (_, { jobId }) => {
+      const job = getSdcardJob(jobId);
+      if (!job) {
+        throw new GraphQLError("Job doesnt exist");
+      }
+
+      if (job.cancelled) {
+        throw new GraphQLError("Job already cancelled");
+      }
+      cancelSdcardJob(jobId);
+
+      return null;
+    },
+    pickSdcardFolder: async () => {
       const handle = await window
         .showDirectoryPicker({
           id: "edgetx-sdcard",
@@ -117,12 +198,16 @@ const resolvers: Resolvers = {
         name: handle.name,
       };
     },
-    createSdcardWriteJob: async (_, { folderId, target }, { github }) => {
-      const handle = directories.find(
+    createSdcardWriteJob: async (
+      _,
+      { folderId, target, clean, sounds },
+      { github }
+    ) => {
+      const directory = directories.find(
         (directory) => directory.id === folderId
-      )?.handle;
+      );
 
-      if (!handle) {
+      if (!directory) {
         throw new GraphQLError("Folder id doesnt exist");
       }
 
@@ -134,76 +219,309 @@ const resolvers: Resolvers = {
         throw new GraphQLError("Target doesnt exist");
       }
 
-      const assetUrl = (
-        await github("GET /repos/{owner}/{repo}/releases/latest")
-      ).data.assets.find((asset) => asset.name === requiredAssetName);
+      const [sdcardAssetUrl, soundsAssetUrl] = await Promise.all([
+        github("GET /repos/{owner}/{repo}/releases/latest", {
+          owner: "EdgeTX",
+          repo: "edgetx-sdcard",
+        }).then(
+          ({ data }) =>
+            data.assets.find((asset) => asset.name === requiredAssetName)
+              ?.browser_download_url
+        ),
+        github("GET /repos/{owner}/{repo}/releases/latest", {
+          owner: "EdgeTX",
+          repo: "edgetx-sdcard-sounds",
+        }).then(
+          ({ data }) =>
+            data.assets.find((asset) =>
+              asset.name.startsWith(`edgetx-sdcard-sounds-${sounds}`)
+            )?.browser_download_url
+        ),
+      ]);
 
-      if (!assetUrl) {
-        throw new GraphQLError("Couldn't find asset url");
+      if (!sdcardAssetUrl) {
+        throw new GraphQLError("Couldn't find sdcard assets url");
       }
 
-      const id = uuid.v1();
+      if (!soundsAssetUrl) {
+        throw new GraphQLError("Couldn't find sound assets url");
+      }
+
+      const job = createSdcardJob(
+        clean ? ["download", "erase", "write"] : ["download", "write"]
+      );
 
       (async () => {
-        const rootDirectory = await window.showDirectoryPicker({
-          id: "edgetx-sdcard",
-        });
-        const contents = await (
-          await ky(`http://localhost:8080/${assetUrl}`, {
-            onDownloadProgress: (progress) => {
-              console.log(progress);
-            },
-          })
-        ).blob();
-        const { entries: zipEntries } = await unzipRaw(contents);
-        const total = zipEntries.reduce((acc, entity) => acc + entity.size, 0);
+        const zipEntries = await download(job.id, [
+          sdcardAssetUrl,
+          soundsAssetUrl,
+        ]);
 
-        let progress = 0;
-        await Promise.all(
-          zipEntries.map(async (file) => {
-            const path = file.name.split("/");
-            const fileName = path[path.length - 1];
-            // ensure the path exists
-            // and recursively head down the tree
-            // to get to the folder where this file needs
-            // to go
-            const fileDirectory = await path
-              .slice(0, path.length - 1)
-              .filter(Boolean)
-              .reduce(
-                async (prev, directory) =>
-                  prev.then(async (parentDirectory) =>
-                    parentDirectory.getDirectoryHandle(directory, {
-                      create: true,
-                    })
-                  ),
-                Promise.resolve(rootDirectory)
-              );
+        if (isCancelled(job.id)) {
+          return;
+        }
 
-            // Only need to save a file if there was something after the folder
-            if (fileName.length > 0) {
-              const fileHandle = await fileDirectory.getFileHandle(fileName, {
-                create: true,
-              });
-              await (await file.blob())
-                .stream()
-                .pipeTo(await fileHandle.createWritable());
+        if (clean && directory.path) {
+          await erase(job.id, directory.path);
+        }
 
-              progress += file.size;
-            }
-          })
-        );
+        if (isCancelled(job.id)) {
+          return;
+        }
+
+        await writeAssets(job.id, directory.handle, zipEntries);
       })();
 
-      return {
-        id,
-      };
+      return job;
     },
   },
 };
 
 const maxDirectoriesHandles = 5;
-const directories: { handle: FileSystemDirectoryHandle; id: string }[] = [];
+const directories: {
+  handle: FileSystemDirectoryHandle;
+  id: string;
+  // when in a node environment (electron main),
+  // we will know the path and can use it for erasing
+  path?: string;
+}[] = [];
+
+const jobUpdates = new PubSub();
+const sdcardJobs: Record<string, SdcardWriteJob> = {};
+
+const createSdcardJob = (
+  stages: (keyof Omit<SdcardWriteJobStages, "__typename">)[]
+) => {
+  const id = uuid.v1();
+  const job: SdcardWriteJob = {
+    id,
+    cancelled: false,
+    stages: Object.fromEntries(
+      stages.map((stage) => [
+        stage,
+        {
+          started: false,
+          completed: false,
+          progress: 0,
+          ...(stage === "download" ? { writes: [] } : undefined),
+        },
+      ])
+    ) as unknown as SdcardWriteJobStages,
+  };
+  sdcardJobs[id] = job;
+  return job;
+};
+
+const getSdcardJob = (jobId: string): SdcardWriteJob | undefined =>
+  sdcardJobs[jobId];
+
+const updateSdcardJob = (jobId: string, updatedJob: SdcardWriteJob) => {
+  sdcardJobs[jobId] = updatedJob;
+  jobUpdates.publish(jobId, updatedJob);
+};
+
+const updateStageStatus = <
+  S extends keyof Omit<SdcardWriteJobStages, "__typename">
+>(
+  jobId: string,
+  stage: S,
+  status: Partial<Omit<NonNullable<SdcardWriteJobStages[S]>, "__typename">>
+) => {
+  const job = getSdcardJob(jobId);
+  if (!job) {
+    return;
+  }
+
+  updateSdcardJob(jobId, {
+    ...job,
+    stages: { ...job.stages, [stage]: { ...job.stages[stage], ...status } },
+  });
+};
+
+const updateSdcardWriteFileStatus = (
+  jobId: string,
+  write: SdcardWriteFileStatus
+) => {
+  const job = getSdcardJob(jobId);
+  updateStageStatus(jobId, "write", {
+    writes: (
+      job?.stages.write.writes.filter(({ name }) => name !== write.name) ?? []
+    ).concat([write]),
+  });
+};
+
+const cancelSdcardJob = (jobId: string) => {
+  const job = getSdcardJob(jobId);
+  if (!job) {
+    return;
+  }
+
+  updateSdcardJob(jobId, { ...job, cancelled: true });
+};
+
+const isCancelled = (jobId: string): boolean => {
+  return getSdcardJob(jobId)?.cancelled ?? true;
+};
+
+/**
+ * Download both of these assets
+ */
+const download = async (
+  jobId: string,
+  assetUrls: string[]
+): Promise<ZipEntry[]> => {
+  updateStageStatus(jobId, "download", {
+    started: true,
+  });
+
+  const downloadProgressTrackers = assetUrls.map(() => ({
+    totalBytes: 0,
+    transferredBytes: 0,
+  }));
+
+  const onDownloadProgress = (key: number, progress: DownloadProgress) => {
+    downloadProgressTrackers[key] = progress;
+    if (
+      downloadProgressTrackers.filter(({ totalBytes }) => totalBytes > 0)
+        .length === assetUrls.length
+    ) {
+      const total = downloadProgressTrackers.reduce(
+        (acc, tracker) => acc + tracker.totalBytes,
+        0
+      );
+      const transferred = downloadProgressTrackers.reduce(
+        (acc, tracker) => acc + tracker.transferredBytes,
+        0
+      );
+
+      updateStageStatus(jobId, "download", {
+        progress: (transferred / total) * 100,
+      });
+    }
+  };
+
+  const assets = await Promise.all(
+    assetUrls.map((assetUrl, i) =>
+      ky(`http://localhost:8080/${assetUrl}`, {
+        onDownloadProgress: (progress) => {
+          onDownloadProgress(i, progress);
+        },
+      }).then((res) => res.blob())
+    )
+  );
+
+  const unzippedAssets = await Promise.all(
+    assets.map((asset) => unzipRaw(asset))
+  );
+
+  updateStageStatus(jobId, "download", {
+    completed: true,
+  });
+
+  return unzippedAssets.reduce(
+    (acc, { entries }) => acc.concat(entries),
+    [] as ZipEntry[]
+  );
+};
+
+const erase = async (jobId: string, path: string): Promise<void> => {
+  updateStageStatus(jobId, "erase", { started: true });
+
+  // Only used in electron
+  const { default: fs } = await import("fs/promises");
+  const entries = await fs.readdir(path);
+  let progress = 0;
+
+  await Promise.all(
+    entries.map(async (entry, i) => {
+      await fs.unlink(`${path}/${entry}`);
+      progress += 1;
+      updateStageStatus(jobId, "erase", {
+        progress: (progress / entries.length) * 100,
+      });
+    })
+  );
+
+  updateStageStatus(jobId, "erase", { completed: true });
+};
+
+const writeAssets = async (
+  jobId: string,
+  rootHandle: FileSystemDirectoryHandle,
+  zipEntries: ZipEntry[]
+): Promise<void> => {
+  updateStageStatus(jobId, "write", { started: true });
+
+  const total = zipEntries.reduce((acc, entity) => acc + entity.size, 0);
+  let progress = 0;
+
+  await Promise.all(
+    zipEntries.map(async (file) => {
+      if (isCancelled(jobId)) {
+        return;
+      }
+
+      const started = new Date().getTime();
+      const path = file.name.split("/");
+      const fileName = path[path.length - 1];
+
+      updateSdcardWriteFileStatus(jobId, {
+        name: file.name,
+        startTime: started,
+      });
+
+      // ensure the path exists
+      // and recursively head down the tree
+      // to get to the folder where this file needs
+      // to go
+      const fileDirectory = await path
+        .slice(0, path.length - 1)
+        .filter(Boolean)
+        .reduce(
+          async (prev, directory) =>
+            prev.then(async (parentDirectory) =>
+              parentDirectory.getDirectoryHandle(directory, {
+                create: true,
+              })
+            ),
+          Promise.resolve(rootHandle)
+        );
+
+      if (isCancelled(jobId)) {
+        return;
+      }
+
+      // Only need to save a file if there was something after the folder
+      if (fileName.length > 0) {
+        const fileHandle = await fileDirectory.getFileHandle(fileName, {
+          create: true,
+        });
+        await file.blob().then(async (blob) => {
+          // Compat for electron main and the browser
+          const stream = blob.stream() as
+            | NodeJS.ReadableStream
+            | ReadableStream;
+          const pipeFunc = "pipe" in stream ? stream.pipe : stream.pipeTo;
+          return pipeFunc(
+            (await fileHandle.createWritable()) as unknown as NodeJS.WritableStream &
+              WritableStream
+          );
+        });
+
+        progress += file.size;
+      }
+
+      updateStageStatus(jobId, "write", { progress: (progress / total) * 100 });
+      updateSdcardWriteFileStatus(jobId, {
+        name: file.name,
+        startTime: started,
+        completedTime: new Date().getTime(),
+      });
+    })
+  );
+
+  updateStageStatus(jobId, "write", { completed: true });
+};
 
 export default {
   typeDefs,
