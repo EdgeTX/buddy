0719c468c993da8e3db35c588e0d0d6ccd8ede34
Oliver Bell <freshollie@gmail.com>
2021-12-09 09:39:29 +0000

    fix webworker

--- src/webworker/backend.bootstrap.ts
+++ src/webworker/backend.bootstrap.ts
@@ -1,6 +1,6 @@
 import { showDirectoryPicker, requestDevice } from "./crossboundary/functions";
 
-const worker = new Worker(new URL("./webworker.worker.ts", import.meta.url));
+const worker = new Worker(new URL("./backend.worker.ts", import.meta.url));
 
 showDirectoryPicker.listen(worker);
 requestDevice.listen(worker);
