b17ea5c6989bc20f784776e9fe65cf74aa412650
Oliver Bell <freshollie@gmail.com>
2022-01-03 18:55:24 +0000

    start working on flashing from PRs

--- src/shared/backend/graph/firmware/index.ts
+++ src/shared/backend/graph/firmware/index.ts
@@ -10,6 +10,8 @@ const typeDefs = gql`
   type Query {
     edgeTxReleases: [EdgeTxRelease!]!
     edgeTxRelease(id: ID!): EdgeTxRelease
+    edgeTxPrs: [EdgeTxPr!]!
+    edgeTxPr(id: ID!): EdgeTxPr
     localFirmware(byId: ID!): LocalEdgeTxFirmware
   }
 
@@ -27,6 +29,19 @@ const typeDefs = gql`
     description: String
     firmwareBundle: EdgeTxFirmwareBundle!
     assets: [EdgeTxReleaseAsset!]!
+  }
+
+  type EdgeTxPr {
+    id: ID!
+    name: String!
+    description: String!
+    commits: [EdgeTxPrCommit!]!
+    headCommitId: String!
+  }
+
+  type EdgeTxPrCommit {
+    id: ID!
+    firmwareBundle: EdgeTxFirmwareBundle
   }
 
   type EdgeTxReleaseAsset {
@@ -114,6 +129,39 @@ const resolvers: Resolvers = {
         })),
       };
     },
+    edgeTxPrs: async (_, __, { github }) => {
+      const prs = await github("GET /repos/{owner}/{repo}/pulls", {
+        owner: config.github.organization,
+        repo: config.github.repos.firmware,
+        sort: "updated",
+        state: "open",
+      });
+
+      return prs.data.map((pr) => ({
+        id: pr.number.toString(),
+        name: pr.head.label,
+        headCommitId: pr.head.sha,
+        commits: [],
+        description: `#${pr.title}\n${pr.body as string}`,
+      }));
+    },
+    edgeTxPr: async (_, { id }, { github }) => {
+      const pr = (
+        await github("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
+          owner: config.github.organization,
+          repo: config.github.repos.firmware,
+          pull_number: Number(id),
+        })
+      ).data;
+
+      return {
+        id: pr.number.toString(),
+        name: pr.head.label,
+        headCommitId: pr.head.sha,
+        commits: [],
+        description: `#${pr.title}\n${pr.body as string}`,
+      };
+    },
     localFirmware: (_, { byId }, { firmwareStore }) => {
       const file = firmwareStore.getLocalFirmwareById(byId);
 
@@ -147,6 +195,7 @@ const resolvers: Resolvers = {
       const firmwareAsset = release.assets.find((asset) =>
         asset.name.includes("firmware")
       );
+
       if (!firmwareAsset) {
         throw new GraphQLError("Error");
       }
@@ -155,6 +204,79 @@ const resolvers: Resolvers = {
         id: firmwareAsset.id,
         name: firmwareAsset.name,
         url: firmwareAsset.url,
+        targets: [],
+      };
+    },
+  },
+  EdgeTxPr: {
+    commits: async ({ id }, _, { github }) => {
+      const commits = await github(
+        "GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
+        {
+          repo: config.github.repos.firmware,
+          owner: config.github.organization,
+          pull_number: Number(id),
+        }
+      );
+
+      return commits.data.map((commit) => ({
+        id: commit.sha,
+        firmwareBundle: null,
+      }));
+    },
+  },
+  EdgeTxPrCommit: {
+    firmwareBundle: async ({ id }, _, { github }) => {
+      const checks = (
+        await github("GET /repos/{owner}/{repo}/commits/{ref}/check-runs", {
+          repo: config.github.repos.firmware,
+          owner: config.github.organization,
+          ref: id,
+        })
+      ).data;
+
+      const githubActionsRun = checks.check_runs.find(
+        (run) =>
+          run.app?.slug === "github-actions" &&
+          run.name.toLowerCase().includes("build")
+      );
+
+      if (!githubActionsRun) {
+        return null;
+      }
+
+      const job = await github(
+        "GET /repos/{owner}/{repo}/actions/jobs/{job_id}",
+        {
+          repo: config.github.repos.firmware,
+          owner: config.github.organization,
+          job_id: githubActionsRun.id,
+        }
+      );
+
+      const { artifacts } = (
+        await github(
+          "GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts",
+          {
+            repo: config.github.repos.firmware,
+            owner: config.github.organization,
+            run_id: job.data.run_id,
+          }
+        )
+      ).data;
+
+      const firmwareAsset = artifacts.find((artifact) =>
+        artifact.name.includes("firmware")
+      );
+
+      if (!firmwareAsset) {
+        return null;
+      }
+
+      return {
+        id: firmwareAsset.id.toString(),
+        name: firmwareAsset.name,
+        url: firmwareAsset.archive_download_url,
         targets: [],
       };
     },
