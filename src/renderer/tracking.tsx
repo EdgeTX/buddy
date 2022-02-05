dcac8353cda8065d8300c340084aacd12fd60106
Oliver Bell <freshollie@gmail.com>
2022-02-05 23:50:30 +0000

    dont use cookies

--- src/renderer/tracking.tsx
+++ src/renderer/tracking.tsx
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import { pageview, initialize } from "react-ga/core";
 import { useLocation } from "react-router-dom";
 
-export const setupTracking = (): void => initialize("G-ZDNY1Z191B");
+export const setupTracking = (): void =>
+  initialize("G-ZDNY1Z191B", {
+    gaOptions: {
+      storage: "none",
+    },
+  });
 
 // eslint-disable-next-line import/prefer-default-export
 export const RouteTracker: React.FC = () => {
