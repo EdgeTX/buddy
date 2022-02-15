30ebfc545b8661e1a2f123555e6355104de4b2f6
Oliver Bell <freshollie@gmail.com>
2022-02-15 15:22:37 +0000

    chore: ensure namespace list remains up to date

--- src/renderer/i18n/config.ts
+++ src/renderer/i18n/config.ts
@@ -1,7 +1,25 @@
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
+import { CustomTypeOptions } from "react-i18next";
 import LazyImportPlugin from "./LazyImportPlugin";
 import languages from "./languages";
+
+const exhaustiveStringTuple =
+  <T extends string>() =>
+  <L extends [T, ...T[]]>(
+    ...x: L extends unknown
+      ? Exclude<T, L[number]> extends never
+        ? L
+        : Exclude<T, L[number]>[]
+      : never
+  ) =>
+    x;
+
+// Ensure that we maintain a list of used namespaces here
+// so that we fetch all namespaces when we switch language
+const namespaces = exhaustiveStringTuple<
+  keyof CustomTypeOptions["resources"]
+>()("common", "compatibility", "sdcard", "flashing");
 
 void i18n
   .use(LazyImportPlugin)
@@ -13,7 +31,7 @@ void i18n
       escapeValue: false,
     },
     defaultNS: "common",
-    ns: ["common", "compatibility", "sdcard", "flashing"],
+    ns: namespaces,
   });
 
 export default i18n;
